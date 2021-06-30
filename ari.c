5faeb2a6a805a038b2f2ef9eb6c792a193334d4e
Jefinks <85078274+Jefinks@users.noreply.github.com>
2021-06-30 22:35:00 +0530

    Update ari.c

---
+++ ari.c
@@ -0,0 +1,68 @@
+#include <ctype.h>
+#include <math.h>
+
+string ari(string s)
+{
+    int characters = 0;
+    int words = 0;
+    int sentences = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isalnum(s[i]))
+            characters++;
+
+        if (s[i] == ' ')
+            words++;
+
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+            sentences++;
+    }
+    int ARI = ceil((4.71 * (float)characters / words) + (0.5 * (float)words / sentences) - 21.43);
+
+    switch (ARI)
+    {
+    case 1:
+        s = "Kindergarten";
+        break;
+    case 2:
+        s = "First/Second Grade";
+        break;
+    case 3:
+        s = "Third Grade";
+        break;
+    case 4:
+        s = "Fourth Grade";
+        break;
+    case 5:
+        s = "Fifth Grade";
+        break;
+    case 6:
+        s = "Sixth Grade";
+        break;
+    case 7:
+        s = " Seventh Grade";
+        break;
+    case 8:
+        s = "Eighth Grade";
+        break;
+    case 9:
+        s = "Ninth Grade";
+        break;
+    case 10:
+        s = "Tenth Grade";
+        break;
+    case 11:
+        s = "Eleventh Grade";
+        break;
+    case 12:
+        s = "Twelfth Grade";
+        break;
+    case 13:
+        s = "College student";
+        break;
+    case 14:
+        s = "Professor";
+        break;
+    }
+    return s;
+}
